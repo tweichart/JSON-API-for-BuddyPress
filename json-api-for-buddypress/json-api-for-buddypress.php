8cb861ed98277db0b5e56f1a35b3c06907203608
Tobias Weichart <tobias.weichart@gmail.com>
2014-02-04 18:14:07 +0100

    * update to 1.0.1

--- json-api-for-buddypress/json-api-for-buddypress.php
+++ json-api-for-buddypress/json-api-for-buddypress.php
@@ -4,7 +4,7 @@
   Plugin Name: JSON API for Buddypress
   Plugin URI: http://wordpress.org/download/json-api-for-buddypress.zip
   Description: Extends the JSON API to be used with Buddypress
-  Version: 1.0
+  Version: 1.0.1
   Author: Tobias Weichart
   Author URI: https://github.com/tweichart/JSON-API-for-BuddyPress
   License: GPL2
